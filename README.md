72d8975d6ddce85c96624851cba6277eec6d2fe3
dansmps <dansampaio977@gmail.com>
2024-09-10 11:30:35 -0300

    Deletei o arquivo objeto.js e criei um novo arquiro README.md.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ADS JavaScript 01
+
+-Códigos relativos as aulas de desenvolvimento dinâmico da faculdade de ADS, com o objetivo de praticar.
--- objeto.js
+++
@@ -1,8 +0,0 @@
-function nomedegente(nome, sobrenome, idade) {
-    return {nome, sobrenome, idade};
-}
-
-const p1 = nomedegente("Daniel", "Sampaio", 24);
-const p2 = nomedegente("Sabrina", "Fernandes", 23);
-
-console.log(p1.nome + ", você aceita se casar com " + p2.nome + " nas suas respectivas idades de " + p1.idade + " e " + p2.idade + " anos?");
